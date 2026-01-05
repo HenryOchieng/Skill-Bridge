2bb578e469a4d67b229267a1d6e63a6e543e7aa3
Henry <henryochieng8@gmail.com>
2026-01-05 18:05:24 +0300

    programs section commit

---
+++ src/Components/Programs.jsx
@@ -0,0 +1,26 @@
+export default function Programs() {
+    return (
+        <section id="programs" className="section">
+            <h3 className="section-title">Programs & Academics</h3>
+            <div className="cards">
+                <div className="card">
+                    <h4>Technology & IT</h4>
+                    <p>
+                        Networking, Cybersecurity, Data Science, Software Development,
+                        Cloud Computing, AI & Machine Learning.
+                    </p>
+                </div>
+                <div className="card">
+                    <h4>Business & Management</h4>
+                    <p>Leadership, entrepreneurship, project management, and digital
+                        business skills
+                    </p>
+                </div>
+                <div className="card">
+                    <h4>Creative Arts & Design</h4>
+                    <p>Industry-recognized certification preparation and short courses.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -2,12 +2,14 @@ import '../Styles/LandingPage.css'
 
 import Header from '../Components/Header.jsx'
 import Hero from '../Components/Hero.jsx'
+import Programs from '../Components/Programs.jsx'
 
 export default function LandingPage() {
     return (
         <>
             <Header />
             <Hero />
+            <Programs />
         </>
     )
 }
