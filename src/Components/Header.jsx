0312370fd79219587dffc524e27924e5c653b131
Henry <henryochieng8@gmail.com>
2026-01-05 17:06:12 +0300

    styling header section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,17 @@
 export default function Header() {
     return (
-        <header>
-            <div className='header-inner'>
-                <div className="logo">Skill Bridge Academy</div>
-                <nav>
-                    <a href="#programs">Programs</a>
-                    <a href="#admissions">Admissions</a>
-                    <a href="#news">News</a>
-                    <a href="contact">Contact</a>
-                </nav>
-            </div>
-        </header>
+        <div className="page">
+            <header>
+                <div className='header-inner'>
+                    <div className="logo">Skill Bridge Academy</div>
+                    <nav className="nav">
+                        <a href="#programs">Programs</a>
+                        <a href="#admissions">Admissions</a>
+                        <a href="#news">News</a>
+                        <a href="contact">Contact</a>
+                    </nav>
+                </div>
+            </header>
+        </div>
     )
 }
